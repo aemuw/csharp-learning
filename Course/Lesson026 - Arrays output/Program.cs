8aff7eff3847ecc315d6f2eeacbc9bead713afeb
Artem <artembohun@gmail.com>
2026-02-22 20:14:52 +0200

    Add lessons 25 and 26

---
+++ Course/Lesson026 - Arrays output/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int[] myArray = { 10, 3, 2, 55 };
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
