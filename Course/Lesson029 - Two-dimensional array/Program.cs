dc294352baa34db91cfa235042823e608f41ebf0
Artem <artembohun@gmail.com>
2026-02-28 01:04:41 +0200

    Add lesson 29

---
+++ Course/Lesson029 - Two-dimensional array/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lesson29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[ , ] matrix = new int[3, 4];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write(matrix[i, j] + "\t"); 
+               Console.WriteLine(); 
+            }
+        }
+    }
+}
