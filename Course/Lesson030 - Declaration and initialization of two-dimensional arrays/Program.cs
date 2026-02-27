2c11a4214709abbd7dc3713242c22d5f74d8f11e
Artem <artembohun@gmail.com>
2026-02-28 01:21:16 +0200

    Add lesson 30

---
+++ Course/Lesson030 - Declaration and initialization of two-dimensional arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace lesson30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //поки тут null
+            int[,] array;
+
+            //тепер цей ярлик вказує на місце в оперативній пам'яті з 3 рядками і 5 стовпцями
+            //всі значення - 0, дефолтне значення при ініціалізації типу int - 0
+            array = new int[3, 5];
+
+            //або int[,] array = new int[3, 5]; 
+
+            array[0, 2] = 5;
+            //array[10, 15] = 4;
+            Console.WriteLine(array[0, 2]);
+
+            int[,] arrayNew = new int[3, 5]
+            {
+                { 5, 1, 5, 8, 0 },
+                { -2, 5, 9, -1, 4 },
+                { 3, 12, 4, -2, -34 }
+            };
+
+            int[,] arrayNew2 = new int[,]
+            {
+                { 5, 1, 5, 8, 0 },
+                { -2, 5, 9, -1, 4 },
+                { 3, 12, 4, -2, -34 }
+            };
+
+            int[,] arrayNew3 =
+            {
+                { 5, 1, 5, 8, 0 },
+                { -2, 5, 9, -1, 4 },
+                { 3, 12, 4, -2, -34 }
+            };
+        }
+    }
+}
