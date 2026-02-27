36ede7d48346f1c22ee673caaa2e37c3b69da9f7
Artem <artembohun@gmail.com>
2026-02-28 01:55:57 +0200

    Add lesson 31

---
+++ Course/Lesson031 - Printing a two-dimensional array/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace lesson31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[,]
+            {
+                { 5, 1, 5, 8, 0 },
+                { -2, 5, 9, -1, 4 },
+                { 3, 12, 4, -2, -34 }
+            };
+
+            /*
+            //пробігається по кожному елементу array та передає значення в item
+            foreach (var item in array)
+                Console.Write(item + " ");
+            */
+
+            //кількість вимірів
+            Console.WriteLine(array.Rank);
+
+            //кількість всіх елементів масиву
+            Console.WriteLine(array.Length);
+
+            //кількість елементів у вимірах масиву
+
+            //кількість рядків
+            Console.WriteLine(array.GetLength(0));
+
+            //кількість елементів в стопці
+            Console.WriteLine(array.GetLength(1));
+
+            //масив не трьовимірний, виникне помилка
+            //Console.WriteLine(array.GetLength(2));
+
+            int height = array.GetLength(0);
+            int width = array.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                    Console.Write(array[i, j] + "\t");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
