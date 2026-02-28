f3dc05aebad962203e145b87218d42151d3445a5
Artem <artembohun@gmail.com>
2026-02-28 15:27:05 +0200

    Add lesson 32

---
+++ Course/Lesson032 - Filling a two-dimensional array from the keyboard/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace lesson32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* заповнення рандомними числами
+            int[,] array = new int[10, 6];
+
+            Random random = new Random();
+
+            int height = array.GetLength(0);
+            int width = array.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    //згенеруються будь-які рандомні числа
+                    //array[i, j] = random.Next();
+
+                    //згенеруютьс рандомні числа від 0 до 100
+                    array[i, j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            /* заповнення з клавіатури
+            int[,] array = new int[2, 3];
+
+            int height = array.GetLength(0);
+            int width = array.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.WriteLine("рядок: " + i + " стовпець: " + j);
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            string[,] array = new string[2, 3];
+
+            int height = array.GetLength(0);
+            int width = array.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.WriteLine("рядок: " + i + " стовпець: " + j);
+                    array[i, j] = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
