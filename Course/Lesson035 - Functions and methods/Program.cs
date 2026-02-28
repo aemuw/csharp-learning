2aeaeab3057279e21d5cc313c2be09f6752123be
Artem <artembohun@gmail.com>
2026-02-28 21:07:12 +0200

    Add lesson 35

---
+++ Course/Lesson035 - Functions and methods/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace lesson35
+{
+    internal class Program
+    {
+        static int Sum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine("Результат додавання: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);
+
+            Console.WriteLine(c);
+            PrintResult(c);
+
+            PrintLine();
+        }
+        static void PrintLine()
+        {
+            Console.WriteLine("Метод PrintLine був визваний!");
+        }
+
+    }
+}
