e626cb657ae3b2c5a89f7e7c6c4a51a490988ec9
Artem <artembohun@gmail.com>
2026-03-03 23:16:28 +0200

    Add lesson 37

---
+++ Course/Lesson037 - Scope/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace lesson37
+{
+    internal class Program
+    {
+        int c = 11;
+        static int d = 2;
+        //static int d = 3;
+
+        static int b = 11;
+        static void Foo()
+        {
+            int b = 9;
+
+            //статичні методи не можуть працювати з нестатичними змінними
+            //c--;
+
+            Console.WriteLine(b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 2;
+
+            for (int i = 0; i < 10; i++)
+            {
+                i++;
+                a--;
+                Program.d--;
+                //int i = 1;
+            }
+            a++;
+            //i++;
+            //b++;
+
+            //Program.Foo();
+            Foo();
+            Program program = new Program();
+            program.c++;
+            
+        }
+    }
+}
