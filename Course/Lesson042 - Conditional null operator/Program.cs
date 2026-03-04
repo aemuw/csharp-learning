d3285ed89b4e95fbab4ef40463777e0d8bd11719
Artem <artembohun@gmail.com>
2026-03-05 00:11:31 +0200

    Add lesson 42

---
+++ Course/Lesson042 - Conditional null operator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace lesson42
+{
+    internal class Program
+    {
+        static int[] GetArray()
+        {
+            int[] arr = null;
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            int[] array = GetArray();
+            /*
+            if (array == null)
+                Console.WriteLine("сума елементів масиву: 0");
+            else
+                Console.WriteLine("сума елементів масиву: " + array.Sum());
+            */
+
+            Console.WriteLine("сума елементів масиву: " + (array?.Sum() ?? 0));
+
+
+        }
+    }
+}
