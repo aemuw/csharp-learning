c2efd333833765fda03e65aefd62906f1d7a1320
Artem <artembohun@gmail.com>
2026-03-04 23:26:10 +0200

    Add lesson 40

---
+++ Course/Lesson40 - Null union operator /Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lesson40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "test";
+            string strn = null;
+             
+            Console.WriteLine(strn ?? "немає даних");
+
+            string result = str ?? string.Empty;
+
+            Console.WriteLine("кількість символів у рядку: " + result.Length);
+        }
+    }
+}
